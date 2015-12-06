b86ca286b6fb65c4460e62850d45a3b9c502eb41
Michael Buesch <m@bues.ch>
2015-12-06 16:45:06 +0100

    hashlib: Add new()

--- hashlib/hashlib/__init__.py
+++ hashlib/hashlib/__init__.py
@@ -1,2 +1,15 @@
 from .sha256 import sha224, sha256
 from .sha512 import sha384, sha512
+
+def new(name, data=b''):
+    try:
+        h = {
+            "sha224" : sha224,
+            "sha256" : sha256,
+            "sha384" : sha384,
+            "sha512" : sha512,
+        }[name.lower()]
+    except KeyError:
+        raise ValueError
+    print(type(h))
+    return h(data)
--- hashlib/test_hashlib.py
+++ hashlib/test_hashlib.py
@@ -1,7 +1,23 @@
 from hashlib.sha256 import test as sha256_test
 from hashlib.sha512 import test as sha512_test
+import hashlib
 
+hashlib.sha224()
+hashlib.sha256()
+hashlib.sha384()
+hashlib.sha512()
 
 sha256_test()
 sha512_test()
+
+assert(isinstance(hashlib.new("sha224"), hashlib.sha224))
+assert(isinstance(hashlib.new("sha256"), hashlib.sha256))
+assert(isinstance(hashlib.new("sha384"), hashlib.sha384))
+assert(isinstance(hashlib.new("sha512"), hashlib.sha512))
+try:
+    hashlib.new("foobar")
+    assert(False)
+except ValueError:
+    pass
+
 print("OK")
